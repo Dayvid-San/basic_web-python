046e242bb1593177d1ed1eba9b54602ce3a3efbe
Dayvid Santana <santana.dayvid@outlook.com>
2022-02-22 17:13:52 -0300

    add message error

--- site.py
+++ site.py
@@ -10,3 +10,10 @@ def index():
 @app.router('/sobre_mim')
 def sobre():
     return render_template("about.html")
+
+
+# "Nome" é o nome da variavel
+@app.router('/<string:nome')
+def error404(nome):
+    variavel = f'Página ({nome}) não existe'
+    return render_template("error.html", vaviavel=variavel)
